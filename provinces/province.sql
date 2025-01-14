ab4202261cc105b337165e4603b00dc78aa25589
wolf-at-alone <amarasekaracharaka2@gmail.com>
2025-01-14 20:12:44 +0530

    added queries for districts and provinces

---
+++ provinces/province.sql
@@ -0,0 +1,10 @@
+INSERT INTO province (province_name)
+VALUES ('Western Province'), 
+       ('Central Province'),
+       ('Southern Province'),
+       ('Northern Province'),
+       ('Eastern Province'),
+       ('North Western Province'),
+       ('North Central Province'),
+       ('Uva Province'),
+       ('Sabaragamuwa Province');
